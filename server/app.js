cce6e6d48cb4f9eae431a98a272394280c7f2023
Sampreeth03 <josyulasivasampreeth@gmail.com>
2026-04-16 22:31:04 +0530

    1

--- server/app.js
+++ server/app.js
@@ -12,7 +12,11 @@ const app = express();
 // ── Middlewares ──────────────────────────────────────────────
 app.use(cors({
     origin: "*",
-  origin: "https://flipkart-clone-peach-six.vercel.app/", 
+    origin: true, // This dynamically reflects the requesting origin
+//   credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+//   origin: "https://flipkart-clone-peach-six.vercel.app/", 
   credentials: true
 }));                   // allow React frontend on different port
 app.use(express.json());            // parse JSON request bodies
