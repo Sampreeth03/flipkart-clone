bb0429b946174d4783e9e8d4d52a5d47a84be65e
Sampreeth03 <josyulasivasampreeth@gmail.com>
2026-04-16 20:25:45 +0530

    final

--- server/server.js
+++ server/server.js
@@ -2,22 +2,7 @@ require("dotenv").config();       // load .env first
 require("./config/db");           // test DB connection on startup
 
 const app  = require("./app");
-// --- DATABASE CONNECTION START ---
-const connection = mysql.createConnection({
-  host: process.env.MYSQLHOST,
-  user: process.env.MYSQLUSER,
-  password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE,
-  port: process.env.MYSQLPORT
-});
 
-connection.connect((err) => {
-  if (err) {
-    console.error('Error connecting to MySQL:', err);
-    return;
-  }
-  console.log('Connected to the global MySQL database!');
-});
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
